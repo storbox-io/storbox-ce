570b46cdb1ec0fd36d0c0cbf648fa87b21172a46
Alexander Mandera <alexander@mandera.eu>
2019-03-28 15:02:49 +0100

    Implement module loading and execution

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 const Core = require('storbox-core').Core;
 
-Core.run(`${__dirname}/config`);
+Core.run(`${__dirname}/config`).then(() => {
+    require('storbox-core').Log.info("Exiting...");
+});
